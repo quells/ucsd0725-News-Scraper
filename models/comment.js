6bb1b6f9e32405c4e1da6531e674e4aace62f805
Kai Wells <support@kaiwells.me>
2017-12-07 18:18:28 -0800

    Comments should not know about articles

--- models/comment.js
+++ models/comment.js
@@ -1,6 +1,5 @@
 module.exports = function(mongoose) {
   return new mongoose.Schema({
-    article: {type: mongoose.Schema.Types.ObjectId, ref: "Article"},
     user: String,
     body: String,
     date: Date
